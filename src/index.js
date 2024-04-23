3e5e38c2e38779cd6c139253ac225245fa9d2bf5
Emmanuel <okponobiemmanuel@gmail.com>
2024-04-23 08:34:13 +0100

    Added cors to backend to work with frontend deployment app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middlewares/error.middleware');
 const connectDB = require('./config/database');
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 app.use('/api/tasks', require('./routes/task.routes'));
 app.use('/api/users', require('./routes/user.routes'));
